b825028907926a96f290827da0e6477ec0860671
adanshah13579 <adanshah13579@gmail.com>
2025-06-25 18:44:23 +0500

    updated slider

--- src/components/homepage components/HeroSlider.jsx
+++ src/components/homepage components/HeroSlider.jsx
@@ -1,13 +1,53 @@
 import Slider from "react-slick";
-import { Box } from "@mui/material";
+import { Box, IconButton } from "@mui/material";
+import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const images = [
-  "./lipstick.jpg",
-  "./lipstick2.jpg", // woman applying lipstick
-  "./lipstick3.jpg", // skincare & perfume layout
-];
+const images = ["./lipstick.jpg", "./lipstick2.jpg", "./lipstick3.jpg"];
+
+// Custom Arrow Components
+const NextArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <IconButton
+      onClick={onClick}
+      sx={{
+        position: "absolute",
+        top: "50%",
+        right: "10px",
+        transform: "translateY(-50%)",
+        zIndex: 1,
+        color: "white",
+        backgroundColor: "rgba(0,0,0,0.4)",
+        "&:hover": { backgroundColor: "rgba(0,0,0,0.6)" },
+      }}
+    >
+      <ArrowForwardIos />
+    </IconButton>
+  );
+};
+
+const PrevArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <IconButton
+      onClick={onClick}
+      sx={{
+        position: "absolute",
+        top: "50%",
+        left: "10px",
+        transform: "translateY(-50%)",
+        zIndex: 1,
+        color: "white",
+        backgroundColor: "rgba(0,0,0,0.4)",
+        "&:hover": { backgroundColor: "rgba(0,0,0,0.6)" },
+      }}
+    >
+      <ArrowBackIos />
+    </IconButton>
+  );
+};
 
 const settings = {
   dots: true,
@@ -17,6 +57,8 @@ const settings = {
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 3000,
+  nextArrow: <NextArrow />,
+  prevArrow: <PrevArrow />,
 };
 
 export default function HeroSlider() {
@@ -25,6 +67,7 @@ export default function HeroSlider() {
       sx={{
         width: "100%",
         height: { xs: "40vh", sm: "60vh", md: "70vh" },
+        position: "relative", // Make sure arrows are positioned inside
         overflow: "hidden",
       }}
     >
